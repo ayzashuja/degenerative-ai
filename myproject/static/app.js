5fd98d50937c95f922ae29f8d1c58682e4b2fe03
talha110-cyber <25100041@lums.edu.pk>
2024-04-12 23:42:12 +0500

    update

--- myproject/static/app.js
+++ myproject/static/app.js
@@ -1,28 +1,3 @@
-// function getGreeting() {
-//     var name = document.getElementById("nameInput").value;
-//     fetch(`/hello?name=${name}`)
-//         .then(response => response.text())
-//         .then(data => {
-//             document.getElementById("greetingOutput").innerText = data;
-//         });
-// }
-
-// function addNumbers() {
-//     var numbers = document.getElementById("numbersInput").value.split(",").map(Number);
-//     fetch('/add', {
-//         method: 'POST',
-//         headers: {
-//             'Content-Type': 'application/json'
-//         },
-//         body: JSON.stringify({ "numbers": numbers })
-//     })
-//         .then(response => response.json())
-//         .then(data => {
-//             document.getElementById("sumOutput").innerText = `Sum: ${data.result}`;
-//         });
-// }
-
-
 function signup() {
     var username = document.getElementById("signupUsername").value;
     var password = document.getElementById("signupPassword").value;
